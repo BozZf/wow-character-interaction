122c4821e01c1d82bfd6cd8e9fbf729ef1636e98
Felipe Bozzoni <fe_bozzoni.90@hotmail.com>
2024-06-21 16:52:05 -0300

    Initial config commit

---
+++ src/main/java/me/dio/wow_character_interaction/WowCharacterInteractionApplication.java
@@ -0,0 +1,13 @@
+package me.dio.wow_character_interaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowCharacterInteractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowCharacterInteractionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/wow_character_interaction/WowCharacterInteractionApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.wow_character_interaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowCharacterInteractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
